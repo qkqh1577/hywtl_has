0f90be6962e1ab7ee9438345666784a88050e97d
양영오(hwc) <young@howoocast.com>
2022-03-23 12:09:58 +0900

    기초 react 뼈대 구성

--- app/src/main/java/com/howoocast/hywtl_has/department/domain/DepartmentCategory.java
+++ app/src/main/java/com/howoocast/hywtl_has/department/domain/DepartmentCategory.java
@@ -1,10 +1,48 @@
 package com.howoocast.hywtl_has.department.domain;
 
+import lombok.Getter;
+
+/**
+ * 부서 유형
+ */
 public enum DepartmentCategory {
-    COMPANY, // 회사
-    HQ, // 본부
-    TEAM, // 팀
-    PART, // 부
-    PERSON, // 개인
-    EXTRA // 기타
+    /**
+     * 회사
+     */
+    COMPANY("회사"),
+
+    /**
+     * 본부
+     */
+    HQ("본부"),
+
+    /**
+     * 팀
+     */
+    TEAM("팀"),
+
+    /**
+     * 부
+     */
+    PART("부"),
+
+    /**
+     * 개인
+     */
+    PERSON("개인"),
+
+    /**
+     * 기타
+     */
+    EXTRA("기타");
+
+    private final String value;
+
+    DepartmentCategory(final String value) {
+        this.value = value;
+    }
+
+    public final String value() {
+        return value;
+    }
 }
