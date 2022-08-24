2e93c4bf8abc34fb7292fa1a57415d122aed614b
lcg-howoo <geun@howoocast.com>
2022-08-24 16:58:15 +0900

    [견적서 내용 관리] 상세 및 등록 수정 화면 form 레이아웃 변경

--- front/admin/estimate/content/view/Detail/Form.tsx
+++ front/admin/estimate/content/view/Detail/Form.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   Box,
   Grid
 } from '@mui/material';
 import TextField from 'components/TextField';
-import SelectField from 'components/SelectField';
 import {
+  EstimateContentVO,
   testTypeList,
   testTypeName
 } from 'admin/estimate/content/domain';
 import DetailList from 'admin/estimate/content/view/Detail/DetailList';
+import CheckboxField from 'components/CheckboxField';
+import {
+  FormikContext,
+  FormikContextType
+} from 'formik';
+import { FormikEditable } from 'type/Form';
 
 export default function Form() {
+  const formikContext: FormikContextType<FormikEditable<EstimateContentVO>> = useContext(FormikContext);
+  const edit = formikContext?.values.edit ?? true;
   return (
     <Box sx={{
       display:  'flex',
@@ -22,25 +30,36 @@ export default function Form() {
         <Grid item sm={12}>
           <h2>실험 정보</h2>
         </Grid>
-        <Grid item sm={6}>
-          <TextField
-            required
-            name="name"
-            label="이름"
-          />
+        <Grid container item sm={4}>
+          <Grid item sm={12}>
+            <TextField
+              required
+              name="name"
+              label="이름"
+            />
+          </Grid>
+          {edit ? (
+            <Grid item sm={12}>
+              <CheckboxField
+                required
+                name="testType"
+                label="실험 타입"
+                options={testTypeList.map((item) => ({
+                  key:  item as string,
+                  text: testTypeName(item)
+                }))}
+              />
+            </Grid>) : (
+            <Grid item sm={12}>
+              <TextField
+                required
+                name="testType"
+                label="실험 타입"
+              />
+            </Grid>
+          )}
         </Grid>
-        <Grid item sm={6}>
-          <SelectField
-            required
-            name="testType"
-            label="실험 타입"
-            options={testTypeList.map((item) => ({
-              key: item as string,
-              text: testTypeName(item)
-            }))}
-          />
-        </Grid>
-        <Grid container spacing={2}>
+        <Grid container item spacing={2}>
           <Grid item sm={12}>
             <h2>내용</h2>
           </Grid>
