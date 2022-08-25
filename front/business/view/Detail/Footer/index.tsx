638669e716e1071d2e1f44a010d2cb197edeea59
lcg-howoo <geun@howoocast.com>
2022-08-25 16:52:52 +0900

    [업체 관리] 삭제 버튼 로직 개발

--- front/business/view/Detail/Footer/index.tsx
+++ front/business/view/Detail/Footer/index.tsx
@@ -6,7 +6,6 @@ import {
   Box,
   Button
 } from '@mui/material';
-import { businessApi } from 'business/api';
 
 function ListButton() {
   const navigate = useNavigate();
@@ -16,20 +15,6 @@ function ListButton() {
   return (
     <Button
       children="목록"
-      onClick={onClick}
-    />
-  );
-}
-
-function RemoveButton(detail) {
-  const formikContext = useContext(FormikContext);
-  const onClick = () => {
-    businessApi.delete(formikContext?.values.id);
-  };
-  return (
-    <Button
-      color="secondary"
-      children="삭제"
       onClick={onClick}
     />
   );
@@ -49,10 +34,13 @@ function EditButton() {
   );
 }
 
-export default function () {
+export interface FooterProps {
+  removeButton: React.ReactNode;
+}
+
+export default function (props: FooterProps) {
   const formikContext = useContext(FormikContext);
   const edit: boolean = formikContext?.values.edit ?? true;
-
   return (
     <DetailFormFooter
       children={!edit
@@ -66,7 +54,7 @@ export default function () {
               width: '50%'
             }}>
               <ListButton />
-              <RemoveButton />
+              {props.removeButton}
             </Box>
             <EditButton />
           </Box>
