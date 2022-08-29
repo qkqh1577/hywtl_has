366a5f061ceb0133b0545a2875c2b6058e3cc89a
lcg-howoo <geun@howoocast.com>
2022-08-29 16:33:52 +0900

    [견적서 관리] 수정사항 반영

--- front/admin/estimate/content/api.ts
+++ front/admin/estimate/content/api.ts
@@ -12,31 +12,31 @@ import { EstimateContentParameter } from 'admin/estimate/content/parameter';
 
 const testShortData: EstimateContentShort[] = [
   {
-    id:          EstimateContentId(1),
-    name:        'test1',
-    testTypeList:    [TestType.A, TestType.E],
-    detailCount: 5
+    id:           EstimateContentId(1),
+    name:         'test1',
+    testTypeList: [TestType.A, TestType.E],
+    detailCount:  5
   },
   {
-    id:          EstimateContentId(2),
-    name:        'test2',
-    testTypeList:    [TestType.A, TestType.B],
-    detailCount: 2
+    id:           EstimateContentId(2),
+    name:         'test2',
+    testTypeList: [TestType.A, TestType.B],
+    detailCount:  2
   },
   {
-    id:          EstimateContentId(3),
-    name:        'test3',
-    testTypeList:    [TestType.COMMON, TestType.REVIEW],
-    detailCount: 3
+    id:           EstimateContentId(3),
+    name:         'test3',
+    testTypeList: [TestType.COMMON, TestType.REVIEW],
+    detailCount:  3
   }
 ];
 
 const testData: EstimateContentVO[] = [
   {
-    id:         EstimateContentId(1),
-    name:       'test1',
-    testTypeList:   [TestType.A, TestType.E],
-    detailList: [
+    id:           EstimateContentId(1),
+    name:         'test1',
+    testTypeList: [TestType.A, TestType.E],
+    detailList:   [
       '건축구조기준(KDS)에 규정된 특별풍하중 조건을 검토한 결과(별첨), 총 x건',
       '(3)항은 풍압실험에서 대상 건축물 외형에 작용하는 풍압을 정밀하게 측정하고, 확률-통계적으로 외장재설계용 풍하중을 평가함',
       '3번 내용물',
@@ -45,19 +45,19 @@ const testData: EstimateContentVO[] = [
     ],
   },
   {
-    id:         EstimateContentId(2),
-    name:       'test2',
-    testTypeList:   [TestType.A, TestType.B],
-    detailList: [
+    id:           EstimateContentId(2),
+    name:         'test2',
+    testTypeList: [TestType.A, TestType.B],
+    detailList:   [
       '건축구조기준(KDS)에 규정된 특별풍하중 조건을 검토한 결과(별첨), 총 x건',
       '(3)항은 풍압실험에서 대상 건축물 외형에 작용하는 풍압을 정밀하게 측정하고, 확률-통계적으로 외장재설계용 풍하중을 평가함'
     ]
   },
   {
-    id:         EstimateContentId(3),
-    name:       'test3',
-    testTypeList:   [TestType.COMMON, TestType.REVIEW],
-    detailList: [
+    id:           EstimateContentId(3),
+    name:         'test3',
+    testTypeList: [TestType.COMMON, TestType.REVIEW],
+    detailList:   [
       '8번 내용, 건축구조기준(KDS)에 규정된 특별풍하중 조건을 검토한 결과(별첨), 총 x건',
       '9번 내용, (3)항은 풍압실험에서 대상 건축물 외형에 작용하는 풍압을 정밀하게 측정하고, 확률-통계적으로 외장재설계용 풍하중을 평가함',
       '10번 내용, (3)항은 풍압실험에서 대상 건축물 외형에 작용하는 풍압을 정밀하게 측정하고, 확률-통계적으로 외장재설계용 풍하중을 평가함',
@@ -90,7 +90,7 @@ const variableTestData: EstimateContentVariableVO[] = [
     name: '{total_price_kor}',
     note: '한글 표기 총 금액(부가세 미포함)',
   }
-]
+];
 
 class EstimateContentApi {
 
@@ -109,10 +109,8 @@ class EstimateContentApi {
   }
 
   async upsert(parameter: EstimateContentParameter): Promise<void> {
-    if (parameter.id) {
-      const { data } = await apiClient.put(`/admin/estimate/content${parameter.id ? `/${parameter.id}` : ''}`, parameter);
-      return data;
-    }
+    const { data } = await apiClient.put(`/admin/estimate/content${parameter.id ? `/${parameter.id}` : ''}`, parameter);
+    return data;
   }
 
   async delete(id: EstimateContentId): Promise<void> {
