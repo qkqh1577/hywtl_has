7070bc706ad843193bd9f3f933d9752e5245138a
lcg-howoo <geun@howoocast.com>
2022-09-13 14:07:32 +0900

    [계약서관리] 계약서 관리 삭제 버그 수정

--- app/src/main/java/com/howoocast/hywtl_has/contract_condition/domain/ContractCondition.java
+++ app/src/main/java/com/howoocast/hywtl_has/contract_condition/domain/ContractCondition.java
@@ -20,7 +20,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Table(name = ContractCondition.KEY)
 @Where(clause = "deleted_at is null")
 @SQLDelete(sql = "update " + ContractCondition.KEY
-    + " set deleted_at = now(), deleted_by = (select u.id from User u where u.username = #{#principal.username}) where id=?")
+    + " set deleted_at = now() where id=?")
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ContractCondition extends CustomEntity {
