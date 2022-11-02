80e20effb9ac2f857d30a8ff93271639a6bd5dcb
이승우(hwc) <woo@howoocast.com>
2022-11-02 18:01:26 +0900

    영업DB분석 화면 PoC 작업중

--- front/project_db/view/Page/filter.tsx
+++ front/project_db/view/Page/filter.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {
     Accordion, AccordionDetails,
     AccordionSummary,
@@ -17,13 +17,57 @@ interface Props {
     schema: ProjectDbSchemaVO[]
 }
 
+interface FilterState {
+    [entity: string]: {
+        checked: boolean,
+        attributes: {
+            [attr: string]: boolean
+        }
+    }
+}
+
 export default function Filter(props: Props) {
     const {schema} = props;
     const entities: string[] = Object.keys(schema);
     const [selectedIndex, setSelectedIndex] = useState(0);
+    const [filterState, setFilterState] = useState<FilterState>({});
 
-    const handleListItemClick = () => {
-        console.warn('implement.....');
+    useEffect(() => {
+        //TODO: LOAD INITIAL FILTER STATE (CURRENT: RANDOM DATA)
+        const initialFilterState = {};
+        entities.map((entityName, index) => {
+            const entityInfo = schema[entityName];
+            const attributes = Object.keys(entityInfo.attributes);
+            const initialAttrState = {};
+            attributes.map((attributeName, attributeIndex) => {
+                const attributeInfo = entityInfo.attributes[attributeName];
+                initialAttrState[attributeName] = attributeIndex % 2 === 0;
+            });
+            initialFilterState[entityInfo.type] = {
+                checked: true,
+                attributes: initialAttrState
+            };
+        });
+        setFilterState(initialFilterState);
+    }, [schema]);
+
+    const onEntityItemChange = (event: React.ChangeEvent<HTMLInputElement>, entityType: string) => {
+        const checked = event.target.checked;
+        const newFilterState = {...filterState};
+        newFilterState[entityType].checked = checked;
+
+        const attributes = Object.keys(newFilterState[entityType].attributes);
+        attributes.forEach((attrName) => {
+            newFilterState[entityType].attributes[attrName] = checked;
+        });
+        setFilterState(newFilterState);
+    };
+
+    const onAttributeItemChange = (event: React.ChangeEvent<HTMLInputElement>, entityType: string, attributeName: string) => {
+        const checked = event.target.checked;
+        const newFilterState = {...filterState};
+        newFilterState[entityType].attributes[attributeName] = checked;
+        setFilterState(newFilterState);
     };
 
     return (
@@ -31,8 +75,11 @@ export default function Filter(props: Props) {
 
             {entities.map((entityName, index) => {
                 const entityInfo = schema[entityName];
+                const entityType = entityInfo.type;
                 const attributes = Object.keys(entityInfo.attributes);
                 const baseKey = (index + 1) * 1000;
+                const entityItemChecked = entityType in filterState && filterState[entityType].checked;
+
                 return (
                     <Accordion key={baseKey}>
                         <AccordionSummary
@@ -40,9 +87,17 @@ export default function Filter(props: Props) {
                         >
                             <Typography>
                                 <Checkbox
+                                    checked={entityItemChecked}
+                                    onClick={(event) => {
+                                        event.stopPropagation();
+                                    }}
+                                    onChange={(event) => {
+                                        onEntityItemChange(event, entityType);
+                                    }}
                                     edge="start"
                                     tabIndex={-1}
                                     disableRipple
+                                    size="small"
                                 />
                                 {entityInfo.description}
                             </Typography>
@@ -51,15 +106,23 @@ export default function Filter(props: Props) {
                             <List>
                                 {attributes.map((attributeName, attributeIndex) => {
                                     const attributeInfo = entityInfo.attributes[attributeName];
+                                    const attrItemChecked = entityItemChecked &&
+                                        filterState[entityType].attributes[attributeName];
+
                                     return (
                                         <ListItemButton
                                             key={baseKey + attributeIndex + 1}
                                         >
                                             <ListItemText>
                                                 <Checkbox
+                                                    checked={attrItemChecked}
+                                                    onChange={(event) => {
+                                                        onAttributeItemChange(event, entityType, attributeName);
+                                                    }}
                                                     edge="start"
                                                     tabIndex={-1}
                                                     disableRipple
+                                                    size="small"
                                                 />
                                                 {attributeInfo.description}
                                             </ListItemText>
