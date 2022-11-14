6b385d77f4415cbfdcb740deadcfe69f30f19f14
lcg-howoo <geun@howoocast.com>
2022-11-14 13:27:27 +0900

    [버그] 프로젝트>자료 : 단지정보 동에 조건에서 형상비검토자료로 사용하는 경우 삭제 불가 및 삭제 로직 구현

--- app/src/main/java/com/howoocast/hywtl_has/project_complex/repository/ProjectComplexBuildingRepository.java
+++ app/src/main/java/com/howoocast/hywtl_has/project_complex/repository/ProjectComplexBuildingRepository.java
@@ -3,8 +3,11 @@ package com.howoocast.hywtl_has.project_complex.repository;
 import com.howoocast.hywtl_has.common.repository.CustomRepository;
 import com.howoocast.hywtl_has.project_complex.domain.ProjectComplexBuilding;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProjectComplexBuildingRepository extends CustomRepository<ProjectComplexBuilding> {
 
     List<ProjectComplexBuilding> findByProject_Id(Long projectId);
+
+    Optional<ProjectComplexBuilding> findByBuildingDocument_Id(Long id);
 }
