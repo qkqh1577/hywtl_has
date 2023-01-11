a6a21e61468d1f6bd447bec2e6e872d1980233e2
이승우(hwc) <woo@howoocast.com>
2023-01-11 22:25:33 +0900

    [공통기능] 로딩 컴포넌트 중첩 로딩 버그수정

--- front/components/CircularProgress.tsx
+++ front/components/CircularProgress.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, CircularProgress as MuiCircularProgress} from '@mui/material';
 
 function CircularProgress(props) {
   return (
     <Box sx={{
       display: 'flex',
       width: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      height:'100%',
+      color: 'grey.500',
+      //backgroundColor: 'rgba(0, 0, 0, 0.5)',
     }}>
-      <CircularProgress />
+      <MuiCircularProgress color="inherit" />
     </Box>
   );
 }
