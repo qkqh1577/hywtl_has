be31916d9f5a780a9ca6c9d80523f5e492b1dd2c
이승우(hwc) <woo@howoocast.com>
2023-01-11 23:17:00 +0900

    [공통기능] 로딩 컴포넌트 옵션 확장(크기,스타일)

--- front/components/CircularProgress.tsx
+++ front/components/CircularProgress.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Box, CircularProgress as MuiCircularProgress} from '@mui/material';
 
 function CircularProgress(props) {
+  const size = (props.size)? props.size : 40;
   return (
     <Box sx={{
+      ...props?.sx,
       display: 'flex',
       width: '100%',
       height:'100%',
       color: 'grey.500',
+      //justifyContent:'center'
       //backgroundColor: 'rgba(0, 0, 0, 0.5)',
     }}>
-      <MuiCircularProgress color="inherit" />
+      <MuiCircularProgress color="inherit" size={size}/>
     </Box>
   );
 }
