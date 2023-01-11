106add118cd35e76999726115902dca6917e03e0
이승우(hwc) <woo@howoocast.com>
2023-01-11 22:28:03 +0900

    [프로젝트] 프로젝트 id 변경 시 기존 상태 초기화 로직 추가

--- front/project_complex/route/index.tsx
+++ front/project_complex/route/index.tsx
@@ -1,41 +1,43 @@
-import { AppRoute } from 'services/routes';
-import React, { useEffect } from 'react';
+import {AppRoute} from 'services/routes';
+import React, {useEffect} from 'react';
 import ProjectContainerRoute from 'project/route/container';
 import ProjectComplexSiteRoute from 'project_complex/route/site';
 import {
-  useDispatch,
-  useSelector
+    useDispatch,
 } from 'react-redux';
 import useId from 'services/useId';
-import { RootState } from 'services/reducer';
-import { projectComplexAction } from 'project_complex/action';
-import { ProjectId } from 'project/domain';
+import {projectComplexAction} from 'project_complex/action';
+import {ProjectId} from 'project/domain';
 import ProjectComplexBuildingRoute from 'project_complex/route/building';
 import ProjectComplexBuildingFileModalRoute from 'project_complex/route/buildingFileModal';
 
 function Element() {
+    const dispatch = useDispatch();
+    const id = useId();
 
-  const dispatch = useDispatch();
-  const id = useId();
-  const { id: projectId } = useSelector((root: RootState) => root.projectComplex);
+    useEffect(() => {
+        return ()=>{
+            dispatch(projectComplexAction.setSiteList(undefined));
+            dispatch(projectComplexAction.setBuildingList(undefined));
+            dispatch(projectComplexAction.setTestDetail(undefined));
+        }
+    }, []);
 
-  useEffect(() => {
-    if (id && projectId !== id) {
-      dispatch(projectComplexAction.setId(ProjectId(id)));
-    }
-  }, [id]);
+    useEffect(() => {
+        id && dispatch(projectComplexAction.setId(ProjectId(id)));
+    }, [id]);
 
-  return (
-    <ProjectContainerRoute>
-      <ProjectComplexSiteRoute />
-      <ProjectComplexBuildingRoute />
-      <ProjectComplexBuildingFileModalRoute />
-    </ProjectContainerRoute>
-  );
+    return (
+        <ProjectContainerRoute>
+            <ProjectComplexSiteRoute/>
+            <ProjectComplexBuildingRoute/>
+            <ProjectComplexBuildingFileModalRoute/>
+        </ProjectContainerRoute>
+    );
 }
 
 const projectComplexRoute: AppRoute = {
-  path:    '/project/sales-management/:id/complex',
-  element: <Element />
+    path: '/project/sales-management/:id/complex',
+    element: <Element/>
 };
 export default projectComplexRoute;
