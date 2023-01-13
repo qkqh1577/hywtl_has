e7145d8345473f5d35ce6e36f2c5dbe76b8e307b
이승우(hwc) <woo@howoocast.com>
2023-01-13 23:34:49 +0900

    [공통기능] 사용자선택기 애니메이션 적용

--- front/components/UserSelector/index.tsx
+++ front/components/UserSelector/index.tsx
@@ -3,19 +3,20 @@ import {
   UserVO
 } from 'user/domain';
 import React, {
+  useCallback,
   useEffect,
   useState
 } from 'react';
 import UserSelectorModal from 'components/UserSelector/Modal';
 import useDialog from 'dialog/hook';
-import { userApi } from 'user/api';
-import Input, { InputProps } from 'layouts/Input';
+import {userApi} from 'user/api';
+import Input, {InputProps} from 'layouts/Input';
 import {
-  Box,
+  Box, Fade,
   InputAdornment
 } from '@mui/material';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { ColorPalette } from 'assets/theme';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {ColorPalette} from 'assets/theme';
 
 interface Props
   extends Omit<InputProps,
@@ -29,75 +30,97 @@ interface Props
   value?: UserId;
 }
 
-export default function UserSelector({ onChange, ...props }: Props) {
+export default function UserSelector({onChange, ...props}: Props) {
 
   const [open, setOpen] = useState<boolean>(false);
-  const onOpen = () => { setOpen(true);};
-  const onClose = () => { setOpen(false);};
-  const { confirm } = useDialog();
+  const onOpen = () => {
+    setOpen(true);
+  };
+  const onClose = useCallback(() => {
+    setOpen(false);
+  }, [open]);
+  const {confirm} = useDialog();
   const value: UserId | undefined = props.value;
   const [user, setUser] = useState<UserVO>();
   useEffect(() => {
     if (value) {
       userApi.getOne(value)
-             .then(setUser);
-    }
-    else {
+        .then(setUser);
+    } else {
       setUser(undefined);
     }
   }, [value]);
+
+  const handleUserRemove = useCallback(() => {
+    return () => {
+      if (props.readOnly || props.disabled || !props.value) {
+        return;
+      }
+      confirm({
+        children: '해당 유저를 제외하겠습니까?',
+        confirmText: '제외',
+        afterConfirm: () => {
+          onChange(undefined);
+        }
+      });
+    };
+  }, [props]);
+
+  const handleUserAdd = useCallback(() => {
+    return (e) => {
+      if (props.readOnly || props.disabled) {
+        return;
+      }
+      e.preventDefault();
+      e.stopPropagation();
+      onOpen();
+    };
+  }, [props]);
+
   return (
     <>
       <Input
         {...props}
         readOnly
         value={user?.name ?? ''}
-        onClick={() => {
-          if (props.readOnly || props.disabled || !props.value) {
-            return;
-          }
-          confirm({
-            children:     '해당 유저를 제외하겠습니까?',
-            confirmText:  '제외',
-            afterConfirm: () => {
-              onChange(undefined);
-            }
-          });
-        }}
+        onClick={handleUserRemove()}
         startAdornment={
           user && (
             <InputAdornment
               position="start"
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-              }}>
+            >
               <Box sx={{
-                display:         'flex',
-                fontSize:        '18px',
-                justifyContent:  'center',
-                alignItems:      'flex-end',
-                overflow:        'hidden',
-                border:          `1px solid ${ColorPalette._e4e9f2}`,
-                color:           ColorPalette._ffffff,
+                display: 'flex',
+                fontSize: '18px',
+                justifyContent: 'center',
+                alignItems: 'flex-end',
+                overflow: 'hidden',
+                border: `1px solid ${ColorPalette._e4e9f2}`,
+                color: ColorPalette._ffffff,
                 backgroundColor: ColorPalette._e4e9f2,
-                borderRadius:    '25px',
-                width:           '25px',
-                height:          '25px',
-                cursor:          'default',
+                borderRadius: '25px',
+                width: '25px',
+                height: '25px',
+                cursor: 'default',
               }}>
                 {user.profile?.id && (
-                  <img
-                    alt="프로필 사진"
-                    src={`/file-item/${user.profile.id}`}
-                    style={{
-                      objectFit: 'cover',
-                      width:     '25px',
-                      height:    '25px',
-                    }}
-                  />
+                  <Fade in={true}>
+                    <img
+                      alt="프로필 사진"
+                      src={`/file-item/${user.profile.id}`}
+                      style={{
+                        objectFit: 'cover',
+                        width: '25px',
+                        height: '25px',
+                      }}
+                    />
+                  </Fade>
                 )}
-                {!user.profile?.id && (<FontAwesomeIcon icon="user" />)}
+                {!user.profile?.id && (
+                  <Fade in={true}>
+                    <FontAwesomeIcon icon="user"/>
+                  </Fade>
+                )}
               </Box>
             </InputAdornment>
           )
@@ -108,18 +131,11 @@ export default function UserSelector({ onChange, ...props }: Props) {
               position="end"
               sx={{
                 fontSize: '16px',
-                cursor:   props.disabled ? 'default' : 'pointer',
+                cursor: props.disabled ? 'default' : 'pointer',
               }}>
               <FontAwesomeIcon
                 icon="user"
-                onClick={(e) => {
-                  if (props.readOnly || props.disabled) {
-                    return;
-                  }
-                  e.preventDefault();
-                  e.stopPropagation();
-                  onOpen();
-                }}
+                onClick={handleUserAdd()}
               />
             </InputAdornment>
           )
