6fba7d1e14b0ed5f9bc056eec6f33d6cc734b7a3
이승우(hwc) <woo@howoocast.com>
2023-01-13 23:54:00 +0900

    [공통기능] Confirm dialog 불필요 스크롤바 생성 문제 처리

--- front/dialog/view/Confirm.tsx
+++ front/dialog/view/Confirm.tsx
@@ -5,16 +5,16 @@ import {
   DialogContent,
   DialogTitle
 } from '@mui/material';
-import React from 'react';
-import { DefaultFunction } from 'type/Function';
+import React, {useCallback} from 'react';
+import {DefaultFunction} from 'type/Function';
 import {
   ConfirmVO,
   DialogStatus
 } from 'dialog/domain';
 import Button from 'layouts/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import TextBox from 'layouts/Text';
-import { ColorPalette } from 'assets/theme';
+import {ColorPalette} from 'assets/theme';
 
 interface Props
   extends ConfirmVO {
@@ -25,39 +25,52 @@ interface Props
 
 export default function Confirm(props: Props) {
 
+  const getButtonColor = useCallback(() => {
+    if (props.status === DialogStatus.ERROR) {
+      return 'error';
+    } else {
+      if (props.status === DialogStatus.WARN) {
+        return 'warning';
+      } else {
+        return undefined;
+      }
+    }
+  }, [props]);
+
   return (
     <Dialog
       open={props.open}
       onClose={props.onClose}
+      fullWidth={true}
+      maxWidth="sm"
       sx={{
         '& .MuiDialog-paper': {
           borderRadius: 0,
         }
       }}>
       <DialogTitle sx={{
-        display:         'flex',
-        alignItems:      'center',
-        justifyContent:  'space-between',
-        minWidth:        '25vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         backgroundColor: ColorPalette._2d3a54,
-        color:           ColorPalette._ffffff,
-        height:          '50px',
-        padding:         '0 20px',
+        color: ColorPalette._ffffff,
+        height: '50px',
+        padding: '0 20px',
       }}>
         <Box sx={{
-          display:        'flex',
+          display: 'flex',
           justifyContent: 'space-between',
-          width:          '100%',
-          flexWrap:       'wrap',
-          alignContent:   'center',
-          alignItems:     'center',
+          width: '100%',
+          flexWrap: 'wrap',
+          alignContent: 'center',
+          alignItems: 'center',
         }}>
           <TextBox variant="heading2">{props.title}</TextBox>
           <FontAwesomeIcon
             icon="xmark"
             onClick={props.onClose}
             style={{
-              cursor:   'pointer',
+              cursor: 'pointer',
               fontSize: '18px',
             }}
           />
@@ -65,27 +78,23 @@ export default function Confirm(props: Props) {
       </DialogTitle>
       <DialogContent>
         <Box sx={{
-          flexWrap:   'wrap',
-          display:    'flex',
-          width:      '100%',
-          paddingTop: '20px',
+          flexWrap: 'wrap',
+          display: 'flex',
+          width: '100%',
+          paddingTop: '30px',
+          justifyContent: 'center'
         }}>
           {props.children}
         </Box>
       </DialogContent>
       <DialogActions sx={{
         justifyContent: 'center',
-        paddingBottom:  '20px',
+        paddingBottom: '20px',
       }}>
         <Button
           onClick={props.onConfirm}
           color={
-            props.status === DialogStatus.ERROR
-              ? 'error'
-              :
-              props.status === DialogStatus.WARN
-                ? 'warning'
-                : undefined
+            getButtonColor()
           }>
           {props.confirmText}
         </Button>
