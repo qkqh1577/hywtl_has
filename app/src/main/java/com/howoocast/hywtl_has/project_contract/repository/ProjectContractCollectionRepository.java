4c47d4a899e15d138f041a90ea80700d8b4aaba0
lcg-howoo <geun@howoocast.com>
2023-01-19 10:48:52 +0900

    필요없는 파일 삭제

--- app/src/main/java/com/howoocast/hywtl_has/project_contract/repository/ProjectContractCollectionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.howoocast.hywtl_has.project_contract.repository;
-
-import com.howoocast.hywtl_has.common.repository.CustomRepository;
-import com.howoocast.hywtl_has.project_contract.domain.ProjectContractCollection;
-
-public interface ProjectContractCollectionRepository extends CustomRepository<ProjectContractCollection> {
-
-}
