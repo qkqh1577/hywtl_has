cffd443671138ca93e2ad0f415644ef2387587ae
lcg-howoo <geun@howoocast.com>
2023-01-31 09:20:22 +0900

    인사카드 필수 값 관련 로직 삭제(기획에 없는 내용 추가된 것임)

--- app/src/main/java/com/howoocast/hywtl_has/personnel/parameter/PersonnelBasicParameter.java
+++ app/src/main/java/com/howoocast/hywtl_has/personnel/parameter/PersonnelBasicParameter.java
@@ -1,10 +1,7 @@
 package com.howoocast.hywtl_has.personnel.parameter;
 
 import com.howoocast.hywtl_has.file.parameter.FileItemParameter;
-import com.howoocast.hywtl_has.personnel.domain.PersonnelBasic;
 import java.time.LocalDate;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -13,14 +10,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Setter
 public class PersonnelBasicParameter {
 
-    @NotBlank(message = PersonnelBasic.KEY + ".eng_name.not_blank")
     private String engName;
 
-    @NotNull(message = PersonnelBasic.KEY + ".birth_date.not_null")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
-    @NotBlank(message = PersonnelBasic.KEY + ".sex.not_blank")
     private String sex;
 
     private FileItemParameter image;
--- app/src/main/java/com/howoocast/hywtl_has/personnel/parameter/PersonnelCompanyParameter.java
+++ app/src/main/java/com/howoocast/hywtl_has/personnel/parameter/PersonnelCompanyParameter.java
@@ -1,10 +1,6 @@
 package com.howoocast.hywtl_has.personnel.parameter;
 
-import com.howoocast.hywtl_has.personnel.domain.PersonnelCompany;
 import java.time.LocalDate;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -13,12 +9,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Setter
 public class PersonnelCompanyParameter {
 
-    @NotNull(message = PersonnelCompany.KEY + ".hired_date.not_null")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate hiredDate;
 
-    @NotBlank(message = PersonnelCompany.KEY + ".hired_type.not_blank")
-    @Pattern(message = "입사 구분은 이하 중 하나만 가능합니다. (신입, 경력)", regexp = "신입|경력")
     private String hiredType;
 
     private String recommender;
