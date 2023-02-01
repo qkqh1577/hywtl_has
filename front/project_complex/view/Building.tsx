6c54fad6fb61b9918cb20713c6d59ac762f9aece
lcg-howoo <geun@howoocast.com>
2023-02-01 16:55:02 +0900

    [단지 정보] 계약에 사용된 견적서 새창 열기 기능 주석 처리

--- front/project_complex/view/Building.tsx
+++ front/project_complex/view/Building.tsx
@@ -39,10 +39,9 @@ import {
   testTypeName
 } from 'type/TestType';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {ProjectContractVO, ProjectFinalContractVO} from 'project_contract/domain';
+import { ProjectFinalContractVO } from 'project_contract/domain';
 import useDialog from 'dialog/hook';
 import { STANDARD_RATIO } from 'util/CommonConstantVariable';
-import CircularProgress from "../../components/CircularProgress";
 
 interface Props {
   list: ProjectComplexBuildingVO[] | undefined;
@@ -91,28 +90,28 @@ export default function ProjectComplexBuildingSection(props: Props) {
             }}>
             + 추가
           </Button>
-          <Button
-            sx={{
-              display: `${!props.contract || !props.contract.id ? 'none' : 'block'}`
-            }}
-            shape="small"
-            onClick={() => {
-              if (!props.contract || !props.contract.id) {
-                error('최종 선택된 계약서가 없습니다.');
-                return;
-              }
-              // TODO: ProjectContractVO 기준으로 로직이 작성되어 있으나, props을 통해 전달된 객체는 ProjectFinalContractVO 임 (임시로 주석처리 함)
-              // if (props.contract.estimate!.type === ProjectEstimateType.SYSTEM) {
-              //   localStorage.setItem('system', '' + props.contract.estimate!.id);
-              // }
-              // else {
-              //   localStorage.setItem('custom', '' + props.contract.estimate!.id);
-              // }
-              props.toEstimateAndContractTab();
-            }}
-          >
-            계약에 사용된 견적서 새창열기
-          </Button>
+          {/*<Button*/}
+          {/*  sx={{*/}
+          {/*    display: `${!props.contract || !props.contract.id ? 'none' : 'block'}`*/}
+          {/*  }}*/}
+          {/*  shape="small"*/}
+          {/*  onClick={() => {*/}
+          {/*    if (!props.contract || !props.contract.id) {*/}
+          {/*      error('최종 선택된 계약서가 없습니다.');*/}
+          {/*      return;*/}
+          {/*    }*/}
+          {/*    // TODO: 기획 변경으로 인한 주석 처리 추후 사용 가능. */}
+          {/*    if (props.contract.estimate!.type === ProjectEstimateType.SYSTEM) {*/}
+          {/*      localStorage.setItem('system', '' + props.contract.estimate!.id);*/}
+          {/*    }*/}
+          {/*    else {*/}
+          {/*      localStorage.setItem('custom', '' + props.contract.estimate!.id);*/}
+          {/*    }*/}
+          {/*    props.toEstimateAndContractTab();*/}
+          {/*  }}*/}
+          {/*>*/}
+          {/*  계약에 사용된 견적서 새창열기*/}
+          {/*</Button>*/}
         </>
       }
       modifiedAt={modifiedAt}
