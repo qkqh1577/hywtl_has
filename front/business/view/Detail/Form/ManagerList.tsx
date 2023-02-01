c1373c6b00b92503d36883e912157209827a40b1
lcg-howoo <geun@howoocast.com>
2023-02-01 10:35:11 +0900

    불필요한 로직 삭제

--- front/business/view/Detail/Form/ManagerList.tsx
+++ front/business/view/Detail/Form/ManagerList.tsx
@@ -13,7 +13,6 @@ import {
   businessManagerStatusName,
 } from 'business/domain';
 import { FormikContext } from 'formik';
-import useDialog from 'dialog/hook';
 import TextBox from 'layouts/Text';
 import Button from 'layouts/Button';
 import { initialBusinessManagerParameter } from 'business/parameter';
@@ -32,11 +31,9 @@ interface Props {
 
 const spaceCount = 4;
 export default function BusinessManagerListSection(props: Props) {
-  const { error } = useDialog();
   const formik = useContext(FormikContext);
   const managerList = formik.values.managerList;
   const edit = formik.values.edit === false ? !formik.values.id : (formik.values.id && formik.values.edit);
-
   return (
     <Box sx={{
       display:  'flex',
